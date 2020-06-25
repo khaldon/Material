397e303407c7f48a4532c77f3111e822a4509f81
khaldon <mohamed.khaled33388@gmail.com>
2020-06-25 20:52:20 +0200

    adding

--- courses/models.py
+++ courses/models.py
@@ -5,7 +5,7 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from slugify import UniqueSlugify
 from django.urls import reverse
-from languages.fields import LanguageField
+# from languages.fields import LanguageField
 
 # Create your models here.
 
@@ -29,7 +29,6 @@ class Course(models.Model):
     updated = models.DateTimeField(auto_now=True)
     category = models.ForeignKey(CourseCategories,on_delete=models.CASCADE)
     certificate = models.ImageField(upload_to='courses/course_certificates',blank=True,null=True)
-    languages = LanguageField(blank=True)
     rank_score = models.FloatField(default=0.0)
     price = models.FloatField(default=0.0)
     discount_price = models.FloatField(blank=True, null=True)
