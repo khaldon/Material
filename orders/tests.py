a39d0e40909281b677cc78c5d94639d5cdbfc69c
khaldon <mohamed.khaled33388@gmail.com>
2020-06-28 14:44:25 +0200

    testing

--- orders/tests.py
+++ orders/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 from .models import OrderCourse
-# Create your tests here.
+from courses.models import Course
+
+
+class TestOrderCourse(TestCase):
+
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username='test1',
+            email='test@gmail.com',
+            password='helloworld',
+        )
+        self.course = Course.objects.get(id=7)
+        self.order_course = OrderCourse.objects.create(user=self.user,
+                                                       ordered=True,
+                                                       course=self.course)
+
+    def test_courses_list_count(self):
+        """Test to count courses."""
+        self.assertEqual(OrderCourse.objects.count(), 1)
+
