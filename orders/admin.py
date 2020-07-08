0b7fa7b68bbe72ecbdad36d20008a004805827f7
Didarbek Nuraliyev <didarbek275@gmail.com>
2020-07-08 15:41:30 +0400

    finished admin page

--- orders/admin.py
+++ orders/admin.py
@@ -2,5 +2,9 @@ from django.contrib import admin
 from orders.models import OrderCourse, Order
 # Register your models here.
 
-admin.site.register(OrderCourse)
-admin.site.register(Order)
+class OrderCourseAdmin(admin.ModelAdmin):
+    list_display = ('user','ordered','course')
+admin.site.register(OrderCourse,OrderCourseAdmin)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('user','ordered','ordered_date')
+admin.site.register(Order,OrderAdmin)
--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Review
 
 # Register your models here.
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('course','reviewer','active','created','updated')
+admin.site.register(Review,ReviewAdmin)
